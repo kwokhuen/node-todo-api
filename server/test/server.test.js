a68719587483ba2369859059b5954abaa5aa8940
kwokhuen <benjaminkwokhuen@gmail.com>
2017-01-24 12:16:39 -0800

    test the delete /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -96,3 +96,39 @@ describe('GET /todos/:id', () => {
       .end(done);
   })
 })
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var id = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${id}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(id);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.findById(id).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch(e => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var id = new ObjectID();
+    request(app)
+      .delete(`/todos/${id.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if objectID is invalid', (done) => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
