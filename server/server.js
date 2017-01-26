9033b41b1ab0c8e80768230361c594e74255b68d
kwokhuen <benjaminkwokhuen@gmail.com>
2017-01-25 17:54:39 -0800

    hashed user password before saving

--- server/server.js
+++ server/server.js
@@ -85,10 +85,8 @@ app.post('/users', (req,res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
   user.save().then(() => {
-    console.log('user:', user);
     return user.generateAuthToken();
   }).then((token) => {
-    console.log('user:', user);
     res.header('x-auth', token).send(user);
   }).catch(e => res.status(400).send(e));
 });
