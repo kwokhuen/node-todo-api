1513fa4f00da5524f07be34bf814f244239d7e55
kwokhuen <benjaminkwokhuen@gmail.com>
2017-01-27 15:58:49 -0800

    add test for POST /users/login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -227,7 +227,7 @@ describe('POST /users', () => {
           expect(user).toExist();
           expect(user.password).toNotBe(password);
           done();
-        });
+        }).catch(e => done(e));
       });
   });
 
@@ -251,3 +251,50 @@ describe('POST /users', () => {
       .end(done);
   });
 });
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    var email = users[1].email;
+    var password = users[1].password;
+    request(app)
+      .post('/users/login')
+      .send({email, password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.header['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.header['x-auth']
+          });
+          done();
+        }).catch(e => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    var email = users[1].email;
+    var password = users[1].password + 1;
+    request(app)
+      .post('/users/login')
+      .send({email, password})
+      .expect(400)
+      .expect((res) => {
+        expect(res.header['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch(e => done(e));
+      });
+  });
+})
